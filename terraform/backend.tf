d4009412e132a5168d7cd177132a3817275857d1
yogi <yogi>
2026-04-07 23:56:59 +0530

    create backend s3 and logs

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "statefile-lab1"
     key            = "s3-frontendlab1"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
