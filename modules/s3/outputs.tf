81feb25b4de98bcc35bd99b86485bfc8bc3ca90c
yogi <yogi>
2026-04-08 12:10:53 +0530

    add cloudfront

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,5 +1,15 @@
 output "bucket_ids" {
+  description = "Map of bucket names"
   value = {
-    for k, v in aws_s3_bucket.s3_backend : k => v.id
+    for key, bucket in aws_s3_bucket.s3_backend :
+    key => bucket.id
+  }
+}
+
+output "bucket_domain_names" {
+  description = "Map of bucket domain names"
+  value = {
+    for key, bucket in aws_s3_bucket.s3_backend :
+    key => bucket.bucket_domain_name
   }
 }
