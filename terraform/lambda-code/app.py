02603f44fb255b02ae82bb7ea5d465b4633692d8
yogi <yogi>
2026-04-10 20:34:54 +0530

    deploy

--- terraform/lambda-code/app.py
+++ terraform/lambda-code/app.py
@@ -1,11 +1,16 @@
-def lambda_handler(event, context):
-    print("Hello from Lambda")
-    return "Success"
+import boto3
 
-try:
-    ...
-except Exception as e:
-    print(str(e))
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    raise Exception("Test DLQ error")
+    print("Lambda started")
+
+    s3.put_object(
+        Bucket='cricket-live-site-dev-logs',
+        Key='output/test.json',
+        Body='Hello from Lambda'
+    )
+
+    print("File written to output bucket")
+
+    return {"status": "success"}
